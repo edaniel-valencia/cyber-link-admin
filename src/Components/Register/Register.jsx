8476d2961054de0468c6905fdff5b71c878ee868
theacademydvr <theacademy.dvr@gmail.com>
2023-10-02 01:12:07 -0500

    registro-web

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css'
 import '../../App.css'
 import { RiLockPasswordLine } from 'react-icons/ri';
@@ -6,13 +6,38 @@ import { AiOutlineMail, AiOutlineUser } from 'react-icons/ai';
 import { IoMdLogIn } from 'react-icons/io';
 import { BsPhone } from 'react-icons/bs';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 // import video from '../../Assets/video.mp4'
 import logo from '../../Assets/cyber-link.png'
 import publicidad from '../../Assets/gamer-1.jpeg'
 
 const Register = () => {
+
+  const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  
+  
+  const createUser = () => {
+    axios.post('http://192.168.1.16:3000/api-tsoftware/users/registerWeb', {
+      name: name,
+      lastname: lastname,
+      email: email,
+      phone: phone,
+      password: password,
+    }).then(() => {
+      console.log('Usuario creado exitosamente');
+    });
+  }
+  
+
+
+
   return (
 
     <div className='registerPage flex'>
@@ -44,36 +69,42 @@ const Register = () => {
             <div className='inputDiv'>
               <div className='input flex'>
                 <AiOutlineUser className='icon' />
-                <input type="text" id='nombre' name="nombre" value="" placeholder='Escribe tus nombres' />
+                <input type="text" id='name' name="name" placeholder='Escribe tus nombres'
+                  onChange={(event)=>{setName(event.target.value)}}/>
               </div>
             </div>
             <div className='inputDiv'>
               <div className='input flex'>
                 <AiOutlineUser className='icon' />
-                <input type="text" id='apellido' name="apellido" value="" placeholder='Escribe tus apellidos' />
+                <input type="text" id='lastname' name="lastname" placeholder='Escribe tus apellidos' 
+                                  onChange={(event)=>{setLastname(event.target.value)}}/>
               </div>
             </div>
             <div className='inputDiv'>
               <div className='input flex'>
                 <BsPhone className='icon' />
-                <input type="number" id='telefono' name="telefono" value="" placeholder='Escribe tu teléfono' />
+                <input type="number" id='phone' name="phone" placeholder='Escribe tu teléfono'
+                                                  onChange={(event)=>{setPhone(event.target.value)}}/>
               </div>
             </div>
             <div className='inputDiv'>
               <div className='input flex'>
                 <AiOutlineMail className='icon' />
-                <input type="email" id='email' name="email" value="" placeholder='Escribe tu correo electrónico' />
+                <input type="email" id='email' name="email" placeholder='Escribe tu correo electrónico'
+                                                  onChange={(event)=>{setEmail(event.target.value)}}/>
               </div>
             </div>
 
             <div className='inputDiv'>
               <div className='input flex'>
                 <RiLockPasswordLine className='icon' />
-                <input type="password" id='password' name="password" value="" placeholder='Escribe una clave' />
+                <input type="password" id='password' name="password" placeholder='Escribe una clave'
+                onChange={(event)=>{setPassword(event.target.value)}}/>
+
               </div>
             </div>
 
-            <button type='submit' className='btn flex'>
+            <button type='submit' className='btn flex' onClick={createUser}>
               <span>Registrarme</span>
               <IoMdLogIn className='icon' />
             </button>
